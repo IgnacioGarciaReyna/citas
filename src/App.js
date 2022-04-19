3799f85b79073791c33d90d5d7fd74b3f4b634f2
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-19 20:48:29 -0300

    Se creó el Grid con Skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+//Para retornar mas de un elemento HTML debo importar Fragment y contener los elementos a retornar dentro de un elemento <Fragment>.
+import React, { Fragment } from "react";
 
 function App() {
   return (
-    <h1>Administrador de pacientes</h1>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      {/* Se utiliza Skeleton para los estilos. 
+      La clase container cumple la misma función que en boostrap */}
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">1</div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
