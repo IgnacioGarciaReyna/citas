5462d3e54b8f9d4560a13effc34017fa9d989a79
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-19 20:48:54 -0300

    Se creó el componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 //Para retornar mas de un elemento HTML debo importar Fragment y contener los elementos a retornar dentro de un elemento <Fragment>.
 import React, { Fragment } from "react";
 
+//Importamos el formulario.js que acabamos de crear
+import Formulario from "./components/Formulario";
+
 function App() {
   return (
     <Fragment>
@@ -9,7 +12,9 @@ function App() {
       La clase container cumple la misma función que en boostrap */}
       <div className="container">
         <div className="row">
-          <div className="one-half column">1</div>
+          <div className="one-half column">
+            <Formulario />
+          </div>
           <div className="one-half column">2</div>
         </div>
       </div>
