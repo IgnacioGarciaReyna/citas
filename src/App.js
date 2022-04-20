794a990ecdd3c786f254e816649875a331fc773b
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-20 19:37:35 -0300

    Se agregaron los campos del formulario

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-//Para retornar mas de un elemento HTML debo importar Fragment y contener los elementos a retornar dentro de un elemento <Fragment>.
 import React, { Fragment } from "react";
-
-//Importamos el formulario.js que acabamos de crear
 import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
-      {/* Se utiliza Skeleton para los estilos. 
-      La clase container cumple la misma función que en boostrap */}
       <div className="container">
         <div className="row">
           <div className="one-half column">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,38 @@
-import React from "react";
+//Extraemos Fragment porque vamos a ponerle mas de un elemento al return
+import React, { Fragment } from "react";
 
 const Formulario = () => {
-  return <h1>Desde formulario</h1>;
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form>
+        <label>Nombre Mascota</label>
+        {/* En HTML hay etiquetas que no tienen cierre. En React tampoco tienen cierre, pero se debe agregar la barra antes de cerrar la etiqueta como pasa con este input. 
+        "u-full-width" es una clase de Eskeleton.
+        */}
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre de la mascota"
+        />
+        <label>Nombre Propietario</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre del dueño"
+        />
+        <label>Fecha</label>
+        <input type="date" name="fecha" className="u-full-width" />
+        <label>Hora</label>
+        <input type="time" name="hora" className="u-full-width" />
+        <label>Síntomas</label>
+        <textarea name="hora" className="u-full-width"></textarea>
+        <button type="submit" className="u-full-width button-primary">Agregar cita</button>
+      </form>
+    </Fragment>
+  );
 };
 
 export default Formulario;
