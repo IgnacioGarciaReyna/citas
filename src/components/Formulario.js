74e68dad8de554389b177789d022c0587b6f188e
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-21 21:52:27 -0300

    Creando el state del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,20 +1,35 @@
-//Extraemos Fragment porque vamos a ponerle mas de un elemento al return
-import React, { Fragment } from "react";
+//Importamos useState que nos permite tener un state en react
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+  //Crear State de Citas
+  //"cita" tiene toda la información del state y "actualizarCita" es la función que reescribe en el objeto, en js lo hariamos con un eventlistener, pero en React tenemos eventos, en este caso onChange.
+  //Creamos un objeto para poder agregar todos los campos que tenemos en el formulario. Vamos a leer cada input por su name.
+  //El objeto inicia con ciertas propiedades. Esas propiedades son strings vacios porque los campos del formulario inician vacios.
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  //Función que se ejecuta cuando el usuario escribe un input
+  const actualizarState = () => {
+    console.log("Escribiendo");
+  };
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
       <form>
         <label>Nombre Mascota</label>
-        {/* En HTML hay etiquetas que no tienen cierre. En React tampoco tienen cierre, pero se debe agregar la barra antes de cerrar la etiqueta como pasa con este input. 
-        "u-full-width" es una clase de Eskeleton.
-        */}
         <input
           type="text"
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de la mascota"
+          onChange={actualizarState}
         />
         <label>Nombre Propietario</label>
         <input
@@ -22,14 +37,31 @@ const Formulario = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre del dueño"
+          onChange={actualizarState}
         />
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+        />
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+        />
         <label>Síntomas</label>
-        <textarea name="hora" className="u-full-width"></textarea>
-        <button type="submit" className="u-full-width button-primary">Agregar cita</button>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
       </form>
     </Fragment>
   );
