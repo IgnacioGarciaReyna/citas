4d6583414b1da1e0dab995c651752a56d5d46870
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-22 23:25:18 -0300

    Se agregó el evento al button

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -9,31 +9,37 @@ const Formulario = () => {
     sintomas: "",
   });
 
-  //Le pasamos el evento a la arrow function, es decir, cada vez que se produce el onChange (cada vez que cambia un campo), se pasa un evento.
-  //Este evento tiene mucha información, por ejemplo, si queres saber en que campo estás escribiendo usas e.target.name, para saber lo que contiene el campo e.target.value.
-  //Usamos actualizarCita que es la función que actualiza el state, no podemos poner cita.mascota = e.target.value por que no sigue las reglas de React.
-  const actualizarState = e => {
-    // console.log(e.target.name);
-    // console.log(e.target.value);
-    
-    //Como es un objeto usamos llaves
-    //Dentro usamos array destructuring y le decimos que la propiedad que sea igual al name va a tener un valor value. 
-    //Utilizamos el spread operator para crear una copia del objeto cita que se creó con el evento anterior.
+  const actualizarState = (e) => {
     actualizarCita({
       ...cita,
-      [e.target.name] : e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   };
 
-  //Extraemos los valores
-  //Utilizamos structuring porque la otra forma sería: const mascota = cita.mascota; const propietario = cita.propietario; etc.
-  //Se recomienda agregar los nombres de las const como value = {'nombreDeLaConst'} en los inputs. Esto nos va a permitir mas adelante resetear el formulario.
-  const {mascota, propietario, fecha, hora, sintomas} = cita
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //Cuando el usuario presiona el agregar cita
+  //onSubmit tiene como default enviar los datos del formulario con el método get y se coloca un query string en la barra de busqueda:"http://localhost:3000/?mascota=&propietario=&fecha=&hora=&sintomas=". Por eso, se escribe un preventDefault al evento, para que no realice esta acción.
+  const submitCita = e => {
+    e.preventDefault();
+    // console.log(e);
+
+    //Validar: Si tuvieramos una base de datos, no insertas primero en la base de datos y despues validas.
+
+    //Asignar un ID: Necesitas un key cuando mostrás registros repetidos en React.
+
+    //Crear la cita
+
+    //Reiniciar el form
+  };
 
   return (
     <Fragment>
       <h2>Crear Cita</h2>
-      <form>
+
+      {/* Vamos a agregar las acciones que realiza el button cuando lo presionamos.
+      En js, cuando tenés un form, usas un addEventListener y luego un submit, lo mismo pasa en React... Debemos agregar un onSubmit en el form y dentro le pasamos una función. */}
+      <form onSubmit={submitCita}>
         <label>Nombre Mascota</label>
         <input
           type="text"
