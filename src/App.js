cef3e2c3c1e8351963c20a4f2f24416c49cdf3a1
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-26 00:10:25 -0300

    Se agregó el almacenamiento en localStorage (1)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Cita from "./components/Cita";
 
 function App() {
   //Citas en local storage
+  //Primero vemos si hay citas en localStorage.
+  //Como localStorage solo almacena strings, vamos a agregar JSON.parse que convierte ese arreglo que está almacenado como string.
   let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  //Si no hay citas iniciales (null) citas iniciales va a ser un arreglo vacio.
   if (!citasIniciales) {
     citasIniciales = [];
   }
 
   //Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  //Una vez que obtuvimos el array de objetos de citasIniciales del localStorage o en su defecto el array vacio, este pasa a ser el valor inicial de el array 'citas' del useState.
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   //useEffect siempre es una arrow function
   //useEffect se ejecuta cuando el componente está listo pero también cuando hay cambios en el componente.
@@ -20,6 +24,8 @@ function App() {
   //Lo vamos a utilizar para colocar las citas en el storage cada vez que se agreguen citas o se eliminen citas, cada vez que el state de citas cambie.
   //Sirve para estar pendiente a ciertos cambios y reaccionar a ellos.
   useEffect(() => {
+    //Acá también tenemos disponible a citasIniciales.
+    //Como tenemos 'citas' como dependecia, cuando se produzca un cambio en el arreglo 'citas' de el state, se las mandamos a citasIniciales del localStorage. Si no, le pasamos un arreglo vacio.
     if (citasIniciales) {
       localStorage.setItem("citas", JSON.stringify(citas));
     } else {
